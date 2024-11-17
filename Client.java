ebed2336d61fa49166c1d22b0b1639c0438b1fca
owensanluis <owenphillipsanluis@gmail.com>
2024-11-17 17:47:03 +0800

    added join and leave commands

--- Client.java
+++ Client.java
@@ -3,36 +3,102 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    private static void handleCommands(String command) {
-        String[] parameters = command.split(" "); //split by spaces
-        String cmd = parameters[0];
+    private static Socket clientEndpoint;
 
-        switch(cmd) {
-            case "/join":
-                break;
-            case "/leave":
-                break;
-            case "/register":
-                break;
-            case "/store":
-                break;
-            case "/dir":
-                break;
-            case "/get":
-                break;
-            case "/?":
-                System.out.println("\nList of commands:");
-                System.out.printf("%-30s - %s\n", "/join <server_ip_add> <port>", "Connect to the server application");
-                System.out.printf("%-30s - %s\n", "/leave", "Disconnect from the server application");
-                System.out.printf("%-30s - %s\n", "/register <handle>", "Register a unique handle or alias");
-                System.out.printf("%-30s - %s\n", "/store <filename>", "Send file to server");
-                System.out.printf("%-30s - %s\n", "/dir", "Request directory file list from a server");
-                System.out.printf("%-30s - %s\n\n", "/get <filename>", "Fetch a file from a server");
-                break;
-            default:
-                System.out.println("Error: Command not found.\n");
-                break;
+    private static boolean handleCommands(String command) {
+        DataOutputStream dosWriter;
+        String[] parameters = command.split(" "); // split by spaces
+        String sServerAddress;
+        int nPort;
 
+        // use try-catch to catch errors
+        try {
+            // if command is /join
+            if(command.startsWith("/join")) {
+                // check if client is already connected to a server
+                if(clientEndpoint == null || clientEndpoint.isClosed()) {
+
+                    //check if correct usage of the /join command
+                    if (parameters.length != 3) {
+                        System.out.println("Error: Command parameters do not match or is not allowed.");
+                        return true;
+                    }
+
+                    // get the parameters of the command
+                    sServerAddress = parameters[1];
+                    nPort = Integer.parseInt(parameters[2]);
+
+                    // try to connect to server
+                    try {
+                        // instantiate the socket, reader, and writer
+                        clientEndpoint = new Socket(sServerAddress, nPort);
+                        final DataInputStream disReader = new DataInputStream(clientEndpoint.getInputStream());
+
+                        System.out.println("Server: " + disReader.readUTF());
+
+                        // listen to server responses in separate threads
+                        // and allow program to continue if server does not have a response for readUTF.
+                        new Thread(() -> getServerResponse(disReader)).start();
+
+                    } catch(IOException e) { // print error message when connection failed
+                        System.out.println("Error: Connection to the Server has failed! " +
+                                            "Please check IP Address and Port Number.");
+                    }
+                } else {
+                    // print an error message if client tries /join even if connected already
+                    System.out.println("Error: Already connected to a server.");
+                }
+            }
+            // if command is /leave
+            else if (command.startsWith("/leave")) {
+                // check if client is connected to a server
+                if (clientEndpoint != null && !clientEndpoint.isClosed()) {
+                    // check if the command usage is correct
+                    // make sure dosWriter is not null so that it will not throw NullPointerException
+                    if (parameters.length == 1) {
+                        dosWriter = new DataOutputStream(clientEndpoint.getOutputStream());
+                        dosWriter.writeUTF("/leave");
+                        clientEndpoint.close();
+                        return false;
+                    } else {
+                        // print error message if command usage is incorrect
+                        System.out.println("Error: Command parameters do not match or is not allowed.");
+                    }
+                } else {
+                    // print error message if client tries /leave without connecting to a server
+                    System.out.println("Error: Disconnection failed. Please connect to the server first.");
+                }
+            }
+            // if command is not /join and /leave AND client is connected to a server
+            else if (clientEndpoint != null && !clientEndpoint.isClosed()) {
+                dosWriter = new DataOutputStream(clientEndpoint.getOutputStream());
+                dosWriter.writeUTF(command);
+            }
+            // if command is not /join and /leave AND client is NOT connected to a server
+            else {
+                System.out.println("Error: Please join a server first.");
+            }
+        } catch (IOException e) {
+            // catch errors and print it
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return true;
+    }
+
+    private static void getServerResponse(DataInputStream disReader) {
+        // try-catch to catch incoming errors
+        try {
+            // loop to keep on reading server's response/message
+            while (true) {
+                // read server's message
+                String serverResponse = disReader.readUTF();
+                // print out the message
+                System.out.println("Server: " + serverResponse);
+            }
+        } catch (IOException e) {
+            // print error message
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
@@ -40,12 +106,14 @@ public class Client {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
+        boolean keepLooping = true;
 
-        while(true) {
+        // loop to get all commands until /leave
+        while(keepLooping) {
             System.out.println("Enter command: ");
             input = sc.nextLine().trim();
 
-            handleCommands(input);
+            keepLooping = handleCommands(input);
         }
     }
 }
--- Server.java
+++ Server.java
@@ -3,4 +3,97 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Server {
+
+    private static void handleClient(Socket clientSocket) {
+        // try-catch to catch any errors
+        try {
+            // instantiate variables
+            DataInputStream disReader = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dosWriter = new DataOutputStream(clientSocket.getOutputStream());
+            String input, cmd;
+            String[] parameters;
+
+            // write the message stating connection is successful
+            dosWriter.writeUTF("Connection to the File Exchange Server is successful!");
+
+            // get the inputs by client
+            input = disReader.readUTF();
+
+            // print inputs by client
+            System.out.println("Server: Received \"" + input + "\" from the client.");
+
+            // split the input by spaces
+            parameters = input.split(" ");
+            cmd = parameters[0];
+
+            switch(cmd) {
+                case "/leave":
+                    dosWriter.writeUTF("Connection closed. Thank you!");
+                    break;
+                case "/register":
+                    break;
+                case "/store":
+                    break;
+                case "/dir":
+                    break;
+                case "/get":
+                    break;
+                case "/?":
+                    System.out.println("\nList of commands:");
+                    System.out.printf("%-30s - %s\n", "/join <server_ip_add> <port>", "Connect to the server application");
+                    System.out.printf("%-30s - %s\n", "/leave", "Disconnect from the server application");
+                    System.out.printf("%-30s - %s\n", "/register <handle>", "Register a unique handle or alias");
+                    System.out.printf("%-30s - %s\n", "/store <filename>", "Send file to server");
+                    System.out.printf("%-30s - %s\n", "/dir", "Request directory file list from a server");
+                    System.out.printf("%-30s - %s\n\n", "/get <filename>", "Fetch a file from a server");
+                    break;
+                default:
+                    dosWriter.writeUTF("Error: Command not found.\n");
+                    break;
+
+            }
+        } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Server: Connection is terminated");
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        Scanner sc = new Scanner(System.in);
+        int nPort;
+        boolean validInput = false;
+
+        while(!validInput) {
+            System.out.println("Enter port number: ");
+
+            try {
+                nPort = sc.nextInt();
+                if (nPort >= 1024 && nPort <= 65535) {
+                    try {
+                        serverSocket = new ServerSocket(nPort);
+                        validInput = true;
+                        System.out.println("Server: Listening on port " + nPort + "...");
+
+                        while (true) {
+                            final Socket serverEndpoint = serverSocket.accept();
+                            System.out.println("Server: New client connected: " +
+                                    serverEndpoint.getRemoteSocketAddress());
+
+                            new Thread(() -> handleClient(serverEndpoint)).start();
+                        }
+
+                    } catch (IOException e) {
+                        System.out.println("Server: ERROR! Port " + nPort +
+                                " is unavailable. Please try another port.\n");
+                    }
+                } else {
+                    System.out.println("Invalid port number. Please enter a number between 1024 and 65535.\n");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter an integer.");
+            }
+        }
+    }
 }
